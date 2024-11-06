5bfd44eca45dbd23cefaa0705b1d82e0aca50ed3
afnan-hmd <91302955+afnan-hmd@users.noreply.github.com>
2024-11-06 08:41:30 +0400

    Update logic - remove unnecessary code

--- script.js
+++ script.js
@@ -4,50 +4,41 @@ const operation = document.querySelectorAll('operation');
 const functionBtn = document.querySelectorAll('functionBtn');
 const equals = document.querySelector('equals');
 
-let previousNum;
+var secInMilliseconds = 1000;
+let previousNum = 0;
 let operator;
-let currentNum;
-
-function add (a, b) {return a + b}
-function subtract (a, b) {return a - b}
-function multiply (a, b) {return a * b}
-function divide (a, b) {
-    if (b == 0) {return 'lmao'}
-    else {return a / b}
-}
+let currentNum = 0;
 
 function operate (a, b, operator) {
     switch (operator) {
         case '+':
-            return add(a, b);
+            return (a + b);
             break;
         case '-':
-            return subtract(a, b);
+            return (a - b);
             break;
         case '*':
-            return multiply(a, b);
+            return (a * b);
             break;
         case '/':
-            return divide(a, b);
+            if (b == 0) {return 'lmao'}
+            else {return a / b}
             break;
     }
 }
 
 function updateDisplay (e) {
     let value = e.target.id;
-    let previousNum = 0;
-    let currentNum = 0;
-    let result;
-    operator;
-    negativeCounter = 1;
 
     if (e.target.classList.contains('operation')) {
         operator = value;
         display.textContent = '';
         display.textContent = operator;
-        let result = operate (Number(previousNum), Number(currentNum), operator);
-        currentNum = result;
-        display.textContent = currentNum;
+
+        currentNum = operate (Number(previousNum), Number(currentNum), operator);
+        setTimeout(() => {
+            display.textContent = currentNum;
+          }, 4 * secInMilliseconds)
     } 
     else if (e.target.classList.contains('functionBtn')) {
         switch (value) {
